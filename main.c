0e54aa7684724ff701348e0d2c8f9c5f93c31f49
JorgeJabczenski <jorgejabczenskiph@gmail.com>
2020-10-14 12:24:48 -0300

    alguns mudanças para conseguir implementar os loops, ainda ta feio

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXMEM 5000
-
+#define MAXMEM  5000
+#define MAXCODE 1000000
 int 
 main(int argc, char **argv)
 {
@@ -24,19 +24,30 @@ main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-	int end = 0;
+	int end = 0, tamCode = 0;
 	char mem[MAXMEM];
+	char source[MAXCODE];
 
 	char c;
 	while((c = fgetc(program)) != EOF)
+		/* Joga todo o código em um vetor */
+		source[tamCode++] = c;
+	tamCode--; /* Ajusta para o tamanho certo */
+
+
+	int i = 0;
+	while(i < tamCode)
 	{
+		c = source[i++];
+
 		if (c == '>') end = (end + 1) % MAXMEM;
 		else if (c == '<') end = (end - 1 + MAXMEM) % MAXMEM;
 		else if (c == '+') mem[end]++;
 		else if (c == '-') mem[end]--;
 		else if (c == '.') printf("%c", mem[end]);
 		else if (c == ',') mem[end] = fgetc(stdin);
-
+		else if (c == '[') printf("Ainda não foi implementado\n");
+		else if (c == ']') printf("Ainda não foi implementado\n");
 	}
 
 	return EXIT_SUCCESS;
